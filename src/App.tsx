a5fbff29a590bd78d97319abe36f33de35a8b8bb
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-09 15:54:57 +0530

    Challenge Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import SplitScreen from "./components/SplitScreen";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+
+      <SplitScreen leftWeight={3} rightWeight={60}>
+        <Sidebar />
+        <Content />
+      </SplitScreen>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
